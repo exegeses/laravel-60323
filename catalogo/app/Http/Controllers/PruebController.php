9b38a164ab8381f872d21c5abbd9b4bdf2af65b2
Marcos <exegeses@gmail.com>
2022-10-22 11:10:58 -0300

    creación de Model y Controller

---
+++ catalogo/app/Http/Controllers/PruebController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebController extends Controller
+{
+    //index
+    public function listarRecurso()
+    {
+    }
+    //show u otro
+    public function verRecursoPorId( $id )
+    {
+    }
+    //create & store
+    public function agregarRecurso(  )
+    {
+    }
+    //edit & update
+    public function modifiarRecurso(  )
+    {
+    }
+    //destroy
+    public function eliminarRecurso()
+    {
+    }
+
+}
