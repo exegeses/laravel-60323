b5f038a9da0fc9654cd5c66191d3b4afe526d7b7
Marcos <exegeses@gmail.com>
2022-10-29 10:42:26 -0300

    implementando paginador de registros

--- catalogo/app/Http/Controllers/CategoriaController.php
+++ catalogo/app/Http/Controllers/CategoriaController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Models\Marca;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
-class MarcaController extends Controller
+class CategoriaController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,10 +14,7 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //obtenemos listado de marcas
-        $marcas = Marca::paginate(7);
-        //retornamos vista
-        return view('marcas', [ 'marcas'=>$marcas ]);
+        //
     }
 
     /**
@@ -45,10 +41,10 @@ class MarcaController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Categoria $categoria)
     {
         //
     }
@@ -56,10 +52,10 @@ class MarcaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Categoria $categoria)
     {
         //
     }
@@ -68,10 +64,10 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Categoria $categoria)
     {
         //
     }
@@ -79,10 +75,10 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Categoria $categoria)
     {
         //
     }
